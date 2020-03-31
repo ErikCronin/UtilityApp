8a42c4cb4e3524a78d931463dd16ab7f4b61f5fc
ErikCronin <erik.cronin@my.jcu.edu.au>
2020-03-31 13:37:30 +1000

    improved main screen, added settings

--- app/src/main/java/com/derk/utilityapp/Settings.java
+++ app/src/main/java/com/derk/utilityapp/Settings.java
@@ -2,35 +2,29 @@ package com.derk.utilityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
+
+    // Settings controls home location, which affects home currency
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
 
         String[] users = { "Australia", "United States", "Japan", "United Kingdom", "France" };
-        Spinner spin = (Spinner) findViewById(R.id.localSpinner);
+        Spinner spin = (Spinner) findViewById(R.id.homeSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
         spin.setAdapter(adapter);
     }
 
-    public void settingsClicked(View view){
-        Intent intent = new Intent(this, Settings.class);
-        startActivity(intent);
+    public void saveClicked(View view){
+        finish();
     }
 
-    public void convertClicked(View view){
 
-    }
-
-    public void confirmClicked(View view){
-
-    }
 }
